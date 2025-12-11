27ad132bd00d3fa516c4c62e52d4e7c5efdef399
Justin <jmsohn92@gmail.com>
2025-12-11 23:57:37 +0000

    you didn't fix it.
    Sohn Enterprises is a privately held U.S. investmen

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -17,11 +17,11 @@ export default function AboutPage() {
       <FadeIn>
         <section className="py-24 md:py-40">
           <div className="container mx-auto px-4">
-            <div className="max-w-4xl mx-auto text-center">
+            <div className="max-w-4xl mx-auto">
               <h1 className="text-3xl md:text-4xl font-light leading-snug tracking-wide">
                 Sohn Enterprises is a privately held U.S. investment syndicate with strategic operations in LATAM.
               </h1>
-              <ul className="mt-20 flex flex-col items-center space-y-8">
+              <ul className="mt-20 flex flex-col space-y-8">
                 {points.map((point, index) => (
                     <li key={index} className="flex items-center w-full max-w-md">
                         <div className="w-px h-6 bg-accent mr-6"></div>
