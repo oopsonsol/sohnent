7b015bf09c26c7c17556e0d6a3149debbc34bdb5
Justin <jmsohn92@gmail.com>
2025-12-13 21:19:39 +0000

    ### Current problems to fix
    1. The live site renders like **unstyled pl

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+// Determines if the deployment is for GitHub Pages
+const isGithubPages = process.env.IS_GITHUB_PAGES === 'true';
+
 const nextConfig = {
+  // Use the repository name as the `basePath` for GitHub Pages deployment
+  basePath: isGithubPages ? '/sohnent' : '',
+  // Use the repository name as the `assetPrefix` for GitHub Pages deployment
+  assetPrefix: isGithubPages ? '/sohnent/' : '',
   output: 'export',
   images: {
     unoptimized: true,
